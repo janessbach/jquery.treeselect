390e2188f730fe660929ef44f16cd13004066f96
Elisa Baum <elisa.baum@adesso.de>
2016-03-11 13:43:17 +0100

    dependencies changed

--- bower_components/jasmine-jquery/Gruntfile.js
+++ bower_components/jasmine-jquery/Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function (grunt) {
       , options: {
           specs: "spec/**/*.js"
         , vendor: "vendor/**/*.js"
-        , version: '2.0.0'
       }
     }
   })
@@ -28,22 +27,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-jasmine')
 
-  grunt.registerTask('syncversion', "Sync the versions between files.", function () {
-    var json = require('./package.json')
-    var fs = require('fs')
-    var file = './lib/jasmine-jquery.js'
-
-    fs.readFile(file, 'utf8', function (err, data) {
-      if (err) return console.log(err)
-
-      var res = data.replace(/^Version .*$/m, 'Version ' + json.version)
-
-      fs.writeFile(file, res, 'utf8', function (err) {
-        if (err) return console.log(err)
-      })
-    })
-  })
-
   grunt.registerTask('test', ['jshint', 'jasmine'])
-  grunt.registerTask('default', ['syncversion', 'test'])
+  grunt.registerTask('default', ['test'])
 };
